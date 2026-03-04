f87898547465bbccd02662faf4938f1045f153e5
gibgibbb <diazkristian321@gmail.com>
2026-03-05 00:13:58 +0800

    Part 2 Sabotaging and Fix

--- app/src/main/java/com/example/mobdevexercise01/MainActivity.java
+++ app/src/main/java/com/example/mobdevexercise01/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.mobdevexercise01;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mCounter = 0;
+    private TextView counterDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +21,21 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().setTitle("ID: 22104031");
         }
 
+        counterDisplay = findViewById(R.id.btnCount);
+        if (counterDisplay != null) {
+            counterDisplay.setText(String.valueOf(mCounter));
+        }
+
+        counterDisplay.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCounter++;
+                if (counterDisplay != null) {
+                    counterDisplay.setText(String.valueOf(mCounter));
+                }
+            }
+        });
+
         Button myButton = findViewById(R.id.btnChangeName);
         final TextView myTextView = findViewById(R.id.tvDepartment);
 
@@ -29,5 +45,19 @@ public class MainActivity extends AppCompatActivity {
                 myTextView.setText("Ms. Angie");
             }
         });
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("COUNT_KEY", mCounter);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mCounter = savedInstanceState.getInt("COUNT_KEY");
+        if (counterDisplay != null) {
+            counterDisplay.setText(String.valueOf(mCounter));
+        }
     }
 }
